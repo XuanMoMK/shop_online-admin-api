4ede7e94908874c4918b619f84be5b896a025008
sunyu <2711783496@qq.com>
2023-11-28 11:58:25 +0800

    分页查询&用户信息导出

--- src/main/java/com/shop_online/service/UserService.java
+++ src/main/java/com/shop_online/service/UserService.java
@@ -1,7 +1,13 @@
 package com.shop_online.service;
 
+import com.shop_online.common.result.PageResult;
 import com.shop_online.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.shop_online.vo.UserVO;
+
+import com.shop_online.common.model.Query;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * <p>
@@ -12,5 +18,22 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-11-28
  */
 public interface UserService extends IService<User> {
+
+    /**
+     * 用户记录
+     *
+     * @param query
+     * @return
+     */
+
+    PageResult<UserVO> getPage (Query query);
+
+    /**
+     * 导出用户记录
+     *
+     * @param query
+     * @param response
+     */
+    void exportUserInfo(Query query, HttpServletResponse response);
 
 }
